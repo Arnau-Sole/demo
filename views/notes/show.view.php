fdbfd25dfef0ed71d4d7a3e1836c5e8b380385d6
Arnau Solé <arnausolereig@gmail.com>
2026-04-06 13:49:30 +0200

    Handle multiple request method

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -4,10 +4,16 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p class="mb-6">
-          <a href="/notes" class="text-blue-500 underline">Go back...</a>
-        </p>
-        <p><?= htmlspecialchars($note["body"]) ?></p>
+		<p class="mb-6">
+		  <a href="/notes" class="text-blue-500 underline">Go back...</a>
+		</p>
+
+		<p><?= htmlspecialchars($note["body"]) ?></p>
+  
+		<form class="mt-6" method="POST">
+			<input type="hidden" name="id" value="<?= $note['id'] ?>">
+			<button class="text-sm text-red-500">Delete</button>
+		</form>  
   </div>
 </main>
 
