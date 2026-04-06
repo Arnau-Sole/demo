25e8982b0dc9fc5757b8deab107eb4dac3366afd
Arnau Solé <arnausolereig@gmail.com>
2026-04-06 13:15:45 +0200

    Namespacing: What, Why, How?

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,9 +1,10 @@
 <?php
 
-require base_path('Validator.php');
+use Core\Database;
+use Core\Validator;
 
 $config = require base_path('config.php');
-$db = new  Database($config['database']);
+$db = new Database($config['database']);
 
 $errors = [];
 
