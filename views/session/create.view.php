bbe0331e5213c4f8e63a738e555b72e412d32dfe
Arnau Solé <arnausolereig@gmail.com>
2026-04-14 20:51:26 +0200

    Flash Old Form Data to the Session

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -13,7 +13,7 @@
 			<div>
 				<label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
 				<div class="mt-2">
-				<input value="<?= $_POST['email'] ?? ''; ?>" id="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+				<input value="<?= old('email') ?>" id="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
 				</div>
 
 				<?php if(isset($errors['email'])) :?>
@@ -26,7 +26,7 @@
 				<label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
 				</div>
 				<div class="mt-2">
-				<input value="<?= $_POST['password'] ?? ''; ?>" id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+				<input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
 				</div>
 
 				<?php if(isset($errors['password'])) :?>
