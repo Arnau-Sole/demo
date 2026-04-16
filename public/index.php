07032ce3c04a58ccc3c68c4df8ccea2037a79474
Arnau Solé <arnausolereig@gmail.com>
2026-04-16 20:55:47 +0200

    Composer and Free Autoloading

--- public/index.php
+++ public/index.php
@@ -5,18 +5,13 @@ use Core\ValidationException;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
-
 const BASE_PATH = __DIR__ . "/../";
 
+require BASE_PATH . 'vendor/autoload.php';
+
+session_start();
+
 require BASE_PATH . 'Core/function.php'; 
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    require base_path("{$class}.php");
-});
-
 
 require base_path('bootstrap.php');
 
